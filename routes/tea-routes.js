46f7f5b43c54947b2a3d03f671f90ca6416f5786
Lauren McNerney <lmcnern@outlook.com>
2015-10-08 20:11:24 -0700

    add tests and route update

--- routes/tea-routes.js
+++ routes/tea-routes.js
@@ -39,6 +39,30 @@ module.exports = function(router) {
       } else {
         res.json({msg: 'location created', id: data._id});
       }
-    })
-  })
+    });
+  });
+  //update existing tea
+  router.put('/tea/:id', function(req, res) {
+    var updateTea = req.body;
+    delete updateTea._id;
+    Tea.findByIdAndUpdate(req.params.id, updateTea, function(err, data) {
+      if (err) {
+        console.log('PUT error ' + err + '.');
+        return res.status(500).json({'msg': 'error!'});
+      } else {
+        res.json(data);
+      }
+      });
+    });
+  //DELETE tea
+  router.delete('/tea/:id', function(req, res) {
+    Tea.remove({_id: req.params.id }, function(err, data) {
+    if (err) {
+      console.log('DELETE error ' + err + '.');
+      return res.status(500).json({'msg': 'error!'});
+    } else {
+      res.json(data);
+    }
+    });
+  });
 }
